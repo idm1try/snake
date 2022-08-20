0b5460b44811f9f816539a0a5ddb843886b0b6f2
idm1try <dimasavchenko1706@icloud.com>
2022-08-20 15:18:24 +0500

    fix: head

--- components/Head.tsx
+++ components/Head.tsx
@@ -2,18 +2,26 @@ import Head from 'next/head';
 
 export const HeadComponent = () => (
   <Head>
-    <meta name='viewport' content='width=device-width, initial-scale=1' />
-    <meta name='description' content='Snake browser game' />
-    <meta name='author' content='idm1try' />
-    <meta name='twitter:title' content='Snake Game' />
-    <meta name='twitter:card' content='summary_large_image' />
-    <meta name='twitter:site' content='@idm1try' />
-    <meta name='twitter:creator' content='@idm1try' />
-    <meta name='twitter:image' content='https://snake-next.vercel.app/preview.png' />
-    <meta property='og:site_name' content='Snake Game' />
-    <meta name='og:title' content='Snake Game' />
-    <meta property='og:type' content='website' />
-    <meta property='og:image' content='https://snake-next.vercel.app/preview.png' />
     <title>Snake</title>
+    <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+    <meta name='author' content='idm1try' />
+    <meta name='description' content='Snake browser game built, using Next.js' />
+    <meta name='keywords' content='snake, snake game, browser game, nextjs, react' />
+
+    <meta property='og:url' content='https://snake-next.vercel.app/' />
+    <meta property='og:type' content='website' />
+    <meta property='og:site_name' content='Snake-Game' />
+    <meta property='og:title' content='Snake-Game' />
+    <meta property='og:description' content='Snake browser game, built using Next.js' />
+    <meta property='og:image' content='https://snake-next.vercel.app/snake.png' />
+    <meta property='og:image:width' content='512' />
+    <meta property='og:image:height' content='512' />
+
+    <meta name='twitter:card' content='summary' />
+    <meta name='twitter:url' content='https://snake-next.vercel.app/' />
+    <meta name='twitter:site' content='@idm1try' />
+    <meta name='twitter:title' content='Snake Game' />
+    <meta name='twitter:description' content='Snake browser game, built using Next.js' />
+    <meta name='twitter:image' content='https://snake-next.vercel.app/snake.png' />
   </Head>
 );
